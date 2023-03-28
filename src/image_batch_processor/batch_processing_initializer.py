babbb3cd9e48916fafc740b6ee24bf5e2cb8d238
Maxim Osipovs <owsipovs@hotmail.de>
2023-03-28 23:09:48 +0200

    updated main function

--- src/image_batch_processor/batch_processing_initializer.py
+++ src/image_batch_processor/batch_processing_initializer.py
@@ -1,5 +1,7 @@
 import argparse
 
+from image_batch_processor.data_collection.image_file_finder import ImageFileFinder
+
 def main():
     parser = argparse.ArgumentParser(
             description="Image batch processor for upscaling and preview picture creation."
@@ -12,11 +14,10 @@ def main():
             )
     parser.add_argument(
             '-m',
-            '--multiplication',
+            '--multiplier',
             help="Specify the upscale multiplier."
                  "If set to None the default multiplicator of 4 will be set",
-            metavar='...', #TODO
-            choices=range(..., ...), #TODO
+            choices=[3, 4]
             type=int,
             default=4
             )
@@ -36,13 +37,12 @@ def main():
             )
     args = parser.parse_args()
 
-    print("### PROCESSING STARTED ###")
-
+    print("### PROCESSING STARTED ###\n")
+    print(f"### PARAMETERS: UPSCALE={args.multiplier}, WATERMARK={args.watermark}, PREVIEW={args.preview} ###\n")
     
+    image_batches = ImageFileFinder.find(args.input)
 
-
-
-
+    print("\n### PROCESSING SUCCESFULLY FINISHED! ###")
 
 if __name__ == "__main__":
     main()
